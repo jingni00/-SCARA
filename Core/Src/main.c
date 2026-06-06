1f8a9ec279fe668ce5f9e1fac82083bb6ca9e906
terry <3125821531@qq.com>
2026-06-06 17:40:14 +0800

    Flip right motor homing direction

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -37,7 +37,7 @@
 #define HOME_RELEASE_MAX_STEPS    3000L
 #define HOME_SEEK_MAX_STEPS       20000L
 #define HOME_SEEK_M1_DIR          -1L
-#define HOME_SEEK_M2_DIR          -1L
+#define HOME_SEEK_M2_DIR          1L
 
 #define MOTOR_EN_ACTIVE           GPIO_PIN_RESET
 #define MOTOR_EN_DISABLE          GPIO_PIN_SET
